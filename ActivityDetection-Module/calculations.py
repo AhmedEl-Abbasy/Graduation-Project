ae230b25b6984cd7b4fcdc1faf278df708024b65
Aya Elghannam <ayatarek9832@gmail.com>
2021-04-15 22:57:30 +0200

    add angle calculation

--- ActivityDetection-Module/calculations.py
+++ ActivityDetection-Module/calculations.py
@@ -1,3 +1,4 @@
+import numpy as np
 right_eye = [[33, 133], [160, 144], [159, 145], [158, 153]] # right eye landmark positions
 left_eye = [[263, 362], [387, 373], [386, 374], [385, 380]] # left eye landmark positions
 
@@ -28,5 +29,15 @@ def eye_feature(landmarks):
     '''
     return (eye_aspect_ratio(landmarks, left_eye) + eye_aspect_ratio(landmarks, right_eye))/2
 
-
-
+def calculate_angle(a,b,c):
+    a = np.array(a) # First
+    b = np.array(b) # Mid
+    c = np.array(c) # End
+    
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    if angle >180.0:
+        angle = 360-angle
+        
+    return angle
