c9451778d5290f8ac69582f8ae3c9b3931c32228
Aya Elghannam <ayatarek9832@gmail.com>
2021-04-16 15:15:29 +0200

    Applied Holistic instead of face_mesh and pose
    we may need the hands landmark for future improvement

--- ActivityDetection-Module/Mediapipe.py
+++ ActivityDetection-Module/Mediapipe.py
@@ -3,12 +3,9 @@ import mediapipe as mp
 import numpy as np
 import calculations , draw
 
-# Declaring FaceMesh model
-mp_face_mesh = mp.solutions.face_mesh
-face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.3, min_tracking_confidence=0.8)
-#Declaring Pose
-mp_pose = mp.solutions.pose
-pose = mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5)
+# Decalring Holistic
+mp_holistic = mp.solutions.holistic
+holistic = mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
 flag = 0
 thresh = 0.2
@@ -21,16 +18,17 @@ while cap.isOpened():
     image.flags.writeable = False
 
     # Process the image
-    face_results = face_mesh.process(image)
-    pose_results = pose.process(image)
+    results = holistic.process(image)
+    face_results = results.face_landmarks
+    pose_results = results.pose_landmarks
 
     image.flags.writeable = True
     image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
     
-    if face_results.multi_face_landmarks:
+    if face_results:
         face_landmarks_positions = []
         # assume that only face is present in the image
-        for _, data_point in enumerate(face_results.multi_face_landmarks[0].landmark):
+        for _, data_point in enumerate(face_results.landmark):
             face_landmarks_positions.append([data_point.x, data_point.y, data_point.z]) 
             # saving normalized landmark positions
         face_landmarks_positions = np.array(face_landmarks_positions)
@@ -38,13 +36,11 @@ while cap.isOpened():
         face_landmarks_positions[:, 1] *= image.shape[0]
 
         # draw face mesh over image
-        for face_landmarks in face_results.multi_face_landmarks:
-            draw.draw_face(face_landmarks,image,mp_face_mesh)
+        draw.draw_face(face_results,image,mp_holistic)
+        # draw pose
+        draw.draw_pose(image,pose_results,mp_holistic)
 
         EAR = calculations.eye_feature(face_landmarks_positions)
-        
-        # draw pose
-        draw.draw_pose(image,pose_results,mp_pose)
 
         ## Return boolean if EAR < thresh
         if EAR < thresh:
@@ -54,19 +50,14 @@ while cap.isOpened():
                 cv.putText(image, "*ALERT!*", (10, 30),cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                 cv.putText(image, "*ALERT!*", (10,325),cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
         else:
-            flag = 0
-
-        draw.draw_pose(image,pose_results,mp_pose)         
+            flag = 0       
         
-    try:
-        landmarks = pose_results.pose_landmarks.landmark
-    except:
-        pass
     
+    landmarks = pose_results.landmark
     # Explain how to extract landmarks Points
-    shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-    elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-    wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+    shoulder = [landmarks[mp_holistic.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_holistic.PoseLandmark.LEFT_SHOULDER.value].y]
+    elbow = [landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].y]
+    wrist = [landmarks[mp_holistic.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_holistic.PoseLandmark.LEFT_WRIST.value].y]
 
     cv.imshow("Frame", image)
     key = cv.waitKey(1) & 0xFF
--- ActivityDetection-Module/draw.py
+++ ActivityDetection-Module/draw.py
@@ -3,16 +3,16 @@ import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils 
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
-def draw_face(face_landmarks,image,mp_face_mesh):
+def draw_face(face_landmarks,image,mp_holistic):
     mp_drawing.draw_landmarks(
                     image=image,
                     landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACE_CONNECTIONS,
+                    connections=mp_holistic.FACE_CONNECTIONS,
                     landmark_drawing_spec=drawing_spec,
                     connection_drawing_spec=drawing_spec)
 
-def draw_pose(image,pose_results,mp_pose):
-    mp_drawing.draw_landmarks(image, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+def draw_pose(image,pose_results,mp_holistic):
+    mp_drawing.draw_landmarks(image, pose_results, mp_holistic.POSE_CONNECTIONS,
                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
                                  )
